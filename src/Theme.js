000401fac6ca279913824d5224728effd4d1a484
ahilesparo <rohaquinlop301@gmail.com>
2021-05-06 18:55:15 -0500

    Lienzo de la pagina

---
+++ src/Theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette : {
+    primary: {
+      main: "#000"
+    },
+    background : {
+      default : "#000000"
+    }
+  }
+});
+
+export default theme;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './Theme';
 
-ReactDOM.render( <App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root"));
